f93926e59cf34b124973e04b6ed20660c75a0613
bonzaza <bonzaza@yandex.com>
2023-11-13 16:26:43 +0300

    Update composer.json change Twig to Blade template engine

--- client/bootstrap/bootstrap.php
+++ client/bootstrap/bootstrap.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use eftec\bladeone\BladeOne;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // Show and log errors
@@ -12,9 +14,12 @@ error_reporting(-1); // E_ALL || -1
 define('ROOT_DIR', dirname(__DIR__));
 
 // Setup template engine
-$loader = new \Twig\Loader\FilesystemLoader(ROOT_DIR . '/views/');
-$twig = new \Twig\Environment($loader, array(
-//    'cache' => false,
-    'cache' => ROOT_DIR . '/storage/cache',
-    'debug' => false, // true,
-));
+$templatesDirectory = ROOT_DIR . '/views/';
+$cacheDirectory = ROOT_DIR . '/storage/cache/';
+
+// From source code:
+// MODE_AUTO - BladeOne reads if the compiled file has changed. If it has changed,then the file is replaced.
+// MODE_SLOW - Then compiled file is always replaced. It's slow and it's useful for development.
+// MODE_FAST - The compiled file is never replaced. It's fast and it's useful for production.
+// MODE_DEBUG - DEBUG MODE, the file is always compiled and the filename is identifiable.
+$blade = new BladeOne($templatesDirectory, $cacheDirectory, BladeOne::MODE_AUTO);
