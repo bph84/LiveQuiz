fa03e1fb4e0851c00010b680778242c825a70a7a
Ben Heley <github-bph@bph.me>
2023-11-13 17:38:45 +0700

    Minor wording change

--- client/public/index.php
+++ client/public/index.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
+
 require_once __DIR__ . '/../bootstrap/boostrap.php';
 
-// load configs
-$configs = require(ROOT_DIR . '/config/config.php');
+require(ROOT_DIR . '/config.php');
 echo $twig->render('index.html', [
     'config' => $configs,
 ]);
