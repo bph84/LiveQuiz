026f104aec800d780b2973fa43336b888f511469
Ben Heley <github-bph@bph.me>
2023-12-27 11:17:04 +0700

    Use .env

--- quiz-taker/public/index.php
+++ quiz-taker/public/index.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../bootstrap/bootstrap.php';
 
-$configs = require(ROOT_DIR . '/config/config.php');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+
+$configs = [
+    'master_key' => $_ENV['MASTER_KEY'],
+    'WS_URL' => $_ENV['WS_URL'],
+    'COOKIE_DOMAIN' => $_ENV['COOKIE_DOMAIN'],
+];
+
 echo $blade->run('index', [
     'config' => $configs,
 ]);
