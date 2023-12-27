fe51dfcf902247a632ec2e986bd9d7ec8f70f82c
Ben Heley <github-bph@bph.me>
2023-12-27 08:34:58 +0700

    Rename the parent directories

---
+++ quiz-setter/config.php
@@ -0,0 +1,7 @@
+<?php
+
+DEFINE ('MASTER_KEY', '362410037571743539825134210820');
+//DEFINE('WS_URL', 'ws://localhost:7071');
+DEFINE('WS_URL', 'wss://ws-quiz.cloud42.uk/wsapp/');
+
+//ws = new WebSocket("wss://ws-quiz.cloud42.uk/wsapp/");
---
+++ quiz-taker/config.php
@@ -0,0 +1,17 @@
+<?php
+
+DEFINE ('MASTER_KEY', '362410037571743539825134210820');
+DEFINE('WS_URL', 'ws://localhost:7071');
+DEFINE('COOKIE_DOMAIN', 'livequiz.bph');
+
+
+$configs = [
+    'master_key' => MASTER_KEY,
+    'WS_URL' => WS_URL,
+    'COOKIE_DOMAIN' => COOKIE_DOMAIN,
+];
+
+//DEFINE('COOKIE_DOMAIN', 'cloud42.uk');
+//DEFINE('WS_URL', 'wss://ws-quiz.cloud42.uk/wsapp/');
+
+//ws = new WebSocket("wss://ws-quiz.cloud42.uk/wsapp/");
